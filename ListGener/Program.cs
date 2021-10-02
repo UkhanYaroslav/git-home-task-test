0be427502f7ad9e6c3cfd183d2da1fea27c94a2b
2panther951@gmail.com <2panther951@gmail.com>
2021-10-02 20:30:57 +0300

    Delegates

--- ListGener/Program.cs
+++ ListGener/Program.cs
@@ -4,40 +4,112 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace UnderstandingLINQ
 {
     class Program
     {
+        delegate void deleg(string x);
+        public delegate void Notify();//delegate for event
+        public delegate void Action_del(string s);
         static void Main(string[] args)
         {
-
-            GenericCollection<Interface1> collection = new GenericCollection<Interface1>();
-
-            Class1 first1 = new Class1() { name = "filip", age = 12 };
-            Class1 first2 = new Class1() { name = "Oleg", age = 20 };
-            Class2 second1 = new Class2() { name = "Lina", surname = "Malik" };
-            Class2 second2 = new Class2() { name = "Maks", surname = "Tris" };
-
-            collection.AddEl(first1);
-            collection.AddEl(first2);
-            collection.AddEl(second1);
-            collection.AddEl(second2);
-
-            foreach (Interface1 obj in collection)
+            string str;
+            while (true)
             {
-                Console.WriteLine(obj.name);
-            }
-            Console.WriteLine();
-            foreach (Interface1 obj in collection)
-            {
-                if (obj.name == "Lina")
+                Console.Write("1.Event   2.Delegate   " + @"3.Action" + "\nYour choose:");
+                string choose = Console.ReadLine();
+                switch (choose)
                 {
-                    Console.WriteLine("Name Lisa = true");
+                    case "1":
+                        class_event class_Event = new class_event();
+                        while (true)
+                        {
+                            str = Console.ReadLine();
+                            class_Event.StartEvent(str);
+                        }
+                    case "2":
+                        deleg d1 = AlphaNumericCollector.method;
+                        deleg d2 = StringCollector.unn;
+                        while (true)
+                        {
+                            str = Console.ReadLine();
+                            if (WithNumber(str))
+                            {
+                                d1(str);
+                            }
+                            else
+                            {
+                                d2(str);
+                            }
+                        }
+                    case "3":
+                        Action_del act_del = Action_method;
+                        while (true)
+                        {
+                            str = Console.ReadLine();
+                            act_del(str);
+                        }
+                    default: Console.WriteLine("Error.Try again"); break;
+                }
+            }
+        }
+        public static bool WithNumber(string stri)//проверка на наличие чисел в строке
+        {
+            foreach (char c in stri)
+            {
+                if (c >= '0' && c <= '9')
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static class AlphaNumericCollector//коллекция со строками с числами
+        {
+            static List<string> num = new List<string>();
+
+            public static void method(string x)
+            {
+                num.Add(x);
+            }
+        }
+        static class StringCollector //коллекция со строками без чисел
+        {
+            static List<string> unnum = new List<string>();
+            public static void unn(string x)
+            {
+                unnum.Add(x);
+            }
+        }
+        public class class_event//Event_class
+        {
+            public event Notify event_deleg;
+            public void StartEvent(string str)
+            {
+                if (WithNumber(str))
+                {
+                    AlphaNumericCollector.method(str);
                 }
                 else
                 {
-                    Console.WriteLine("Name Lisa = false");
+                    StringCollector.unn(str);
                 }
+                OnProcessCompleted();
+            }
+            protected virtual void OnProcessCompleted()
+            {
+                event_deleg?.Invoke();
+            }
+        }
+        static void Action_method(string s) //action method
+        {
+            if (WithNumber(s))
+            {
+                AlphaNumericCollector.method(s);
+            }
+            else
+            {
+                StringCollector.unn(s);
             }
         }
     }
